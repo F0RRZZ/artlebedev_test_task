c345bdff43c9f9c778e8703bc0b94a54e33f8f37
marat <marathagazeev@gmail.com>
2023-09-11 23:41:12 +0300

    Add parser

--- artlebedev_test_task/parks/models.py
+++ artlebedev_test_task/parks/models.py
@@ -3,27 +3,61 @@ from django.db import models
 
 class Park(models.Model):
     name = models.CharField(
-        'name', max_length=100, help_text='Maximum of 100 symbols'
+        'name',
+        max_length=100,
+        help_text='Maximum of 100 symbols',
+        null=True,
+        blank=True,
     )
     description = models.TextField(
-        'description', max_length=1000, help_text='Maximum of 1000 symbols'
+        'description',
+        max_length=1000,
+        help_text='Maximum of 1000 symbols',
+        null=True,
+        blank=True,
     )
     address = models.CharField(
-        'address', max_length=200, help_text='Maximum of 200 symbols'
+        'address',
+        max_length=200,
+        help_text='Maximum of 200 symbols',
+        null=True,
+        blank=True,
     )
     coordinates = models.CharField(
-        'coordinates', max_length=100, help_text='Maximum of 100 symbols'
+        'coordinates',
+        max_length=100,
+        help_text='Maximum of 100 symbols',
+        null=True,
+        blank=True,
     )
-    email = models.EmailField('email', help_text='Maximum of 254 symbols')
+    email = models.EmailField(
+        'email', help_text='Maximum of 254 symbols', null=True, blank=True
+    )
     phone = models.CharField(
-        'phone', max_length=15, help_text='Maximum of 15 symbols'
+        'phone',
+        max_length=15,
+        help_text='Maximum of 15 symbols',
+        null=True,
+        blank=True,
     )
     website = models.CharField(
-        'website', max_length=200, help_text='Maximum of 200 symbols'
+        'website',
+        max_length=200,
+        help_text='Maximum of 200 symbols',
+        null=True,
+        blank=True,
     )
     organization = models.CharField(
-        'organization', max_length=200, help_text='Maximum of 200 symbols'
+        'organization',
+        max_length=200,
+        help_text='Maximum of 200 symbols',
+        null=True,
+        blank=True,
     )
     inn = models.CharField(
-        'inn', max_length=12, help_text='Maximum of 12 symbols'
+        'inn',
+        max_length=12,
+        help_text='Maximum of 12 symbols',
+        null=True,
+        blank=True,
     )
--- artlebedev_test_task/parser.py
+++ artlebedev_test_task/parser.py
@@ -1,8 +1,85 @@
-from pprint import pprint
 import json
+import os
+from pathlib import Path
+
+import environ
+import psycopg2
+
+
+def add_park_to_db(park_info: tuple):
+    cursor.execute(
+        """
+        INSERT INTO parks_park
+        (
+        name,
+        description,
+        address,
+        coordinates,
+        email,
+        phone,
+        website,
+        organization,
+        inn
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """,
+        park_info,
+    )
+    connection.commit()
+
+
+BASE_DIR = Path(__file__).resolve().parent
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+env = environ.Env(
+    POSTGRES_HOST=(str, 'localhost'),
+    POSTGRES_DB=(str, 'postgres'),
+    POSTGRES_PASSWORD=(str, 'password'),
+    POSTGRES_PORT=(str, '5432'),
+    POSTGRES_USER=(str, 'postgres'),
+)
+
+connection = psycopg2.connect(
+    user=env('POSTGRES_USER'),
+    password=env('POSTGRES_PASSWORD'),
+    host=env('POSTGRES_HOST'),
+    port=env('POSTGRES_PORT'),
+    database=env('POSTGRES_DB'),
+)
+cursor = connection.cursor()
 
 with open(
     file='data-85-structure-3.json', mode='r', encoding='utf-8'
 ) as read_file:
     data = json.load(read_file)
-    pprint(data[235]['data'])
+    for park in data:
+        park_name = park['data']['general']['name']
+        description = park['data']['general']['description']
+        address = park['data']['general']['address']['fullAddress']
+        coordinates = park['data']['general']['address']['mapPosition'][
+            'coordinates'
+        ]
+        coordinates = ', '.join(map(str, coordinates))
+        if 'contacts' in park['data']['general']:
+            email = park['data']['general']['contacts'].get('email', '')
+            if park['data']['general']['contacts']['phones']:
+                phone = park['data']['general']['contacts']['phones'][0][
+                    'value'
+                ]
+            else:
+                phone = None
+            website = park['data']['general']['contacts'].get('website')
+        organization = park['data']['general']['organization']['name']
+        inn = park['data']['general']['organization'].get('inn')
+        info = (
+            park_name,
+            description,
+            address,
+            coordinates,
+            email,
+            phone,
+            website,
+            organization,
+            inn,
+        )
+        add_park_to_db(info)
